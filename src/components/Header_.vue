38f953f3483870474c87e3ac85a16d140bbb0bf2
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-04 13:15:49 -0400

    feat: enhance form styling in Header_ component

--- src/components/Header_.vue
+++ src/components/Header_.vue
@@ -5,7 +5,7 @@
     <div class="mx-auto container px-5 py-16">
       <div></div>
 
-      <form></form>
+      <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 py-10 rounded-lg shadow space-y-6"></form>
     </div>
   </header>
 </template>
