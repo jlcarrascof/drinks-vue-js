db8813e43736b1765e0950abaf233879bd9381df
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-04 16:38:54 -0400

    feat: enhance HeaderForm component with layout and routing

--- src/components/HeaderForm.vue
+++ src/components/HeaderForm.vue
@@ -1,9 +1,18 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
-  <header>
+  <header class="bg-slate-800">
     <div class="mx-auto container px-5 py-16">
-      <div></div>
+      <div class="flex justify-between items-center">
+        <div>
+          <RouterLink :to="{ name: 'inicio' }">
+            <img class="w-32" src="/img/logo.svg" alt="logotipo" />
+          </RouterLink>
+        </div>
+        <nav></nav>
+      </div>
 
       <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
         <div class="space-y-4">
