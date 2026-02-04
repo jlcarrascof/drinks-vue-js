5fcf9cbf4d2747e59f8895dfe678550dcde5979f
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-04 13:49:11 -0400

    feat: add form structure and label to Header_ component

--- src/components/Header_.vue
+++ src/components/Header_.vue
@@ -5,7 +5,13 @@
     <div class="mx-auto container px-5 py-16">
       <div></div>
 
-      <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 py-10 rounded-lg shadow space-y-6"></form>
+      <form class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 py-10 rounded-lg shadow space-y-6">
+        <div class="space-y-4">
+          <label class="block text-white uppercase font-extrabold text-lg" for="ingrediente"
+            >Name or Ingredient</label
+          >
+        </div>
+      </form>
     </div>
   </header>
 </template>
