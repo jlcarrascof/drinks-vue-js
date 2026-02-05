64a5fec4d493798acb8e0041eda3b7509731b872
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-05 15:10:42 -0400

    feat: add button to RecetasCard for viewing recipes

--- src/components/RecetasCard.vue
+++ src/components/RecetasCard.vue
@@ -17,6 +17,12 @@ defineProps({
     </div>
     <div class="p-5">
       <h2 class="text-2xl truncate font-extrabold">{{ receta.strDrink }}</h2>
+      <button
+        type="button"
+        class="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg"
+      >
+        View recipe
+      </button>
     </div>
   </div>
 </template>
