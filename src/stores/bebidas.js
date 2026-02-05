fab3e85a6c7993f6b02365260edb1e8e2b513601
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-05 10:32:54 -0400

    feat: fetch and log categorias data on store mount

--- src/stores/bebidas.js
+++ src/stores/bebidas.js
@@ -1,9 +1,15 @@
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
 export const useBebidasStore = defineStore('bebidas', () => {
   const categorias = ref([])
+
+  onMounted(async () => {
+    const data = await axios('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+
+    console.log(data)
+  })
 
   return {
     categorias,
