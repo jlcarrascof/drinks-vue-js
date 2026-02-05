85ce1cd3d55f11eb8cc23a32a58a7deae53f554f
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-05 11:06:21 -0400

    refactor: improve data fetching structure in bebidas store

--- src/stores/bebidas.js
+++ src/stores/bebidas.js
@@ -6,9 +6,11 @@ export const useBebidasStore = defineStore('bebidas', () => {
   const categorias = ref([])
 
   onMounted(async () => {
-    const data = await axios('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+    const {
+      data: { drinks },
+    } = await axios('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
 
-    console.log(data)
+    categorias.value = drinks
   })
 
   return {
