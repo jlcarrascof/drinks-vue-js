184d496a02ae5d04c9cde2e9b47b94c45fb4f2ac
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-05 13:21:45 -0400

    feat: add RecetasCard component and integrate it into InicioView for displaying recipes

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,5 +1,14 @@
-<script setup></script>
+<script setup>
+import Receta from '../components/RecetasCard.vue'
+import { useBebidasStore } from '../stores/bebidas'
+
+const bebidas = useBebidasStore()
+</script>
 
 <template>
   <h1 class="text-6xl font-extrabold">Recipes</h1>
+
+  <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 mt-10 gap-10">
+    <Receta />
+  </div>
 </template>
