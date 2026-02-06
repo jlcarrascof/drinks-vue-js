129a260c266846a57ce9a97a6e3283b513a9901c
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-06 10:43:07 -0400

    feat: add watch and localStorage synchronization for favoritos state

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 import { defineStore } from 'pinia'
 import { useBebidasStore } from './bebidas'
 
@@ -6,6 +6,19 @@ export const useFavoritosStore = defineStore('favoritos', () => {
   const bebidas = useBebidasStore()
   const favoritos = ref([])
 
+  watch(
+    favoritos,
+    () => {
+      sincronizarLocalStorage()
+    },
+    {
+      deep: true,
+    },
+  )
+
+  const sincronizarLocalStorage = () => {
+    localStorage.setItem('favoritos', JSON.stringify(favoritos.value))
+  }
   const handleClickFavorito = () => {
     favoritos.value.push(bebidas.receta)
   }
