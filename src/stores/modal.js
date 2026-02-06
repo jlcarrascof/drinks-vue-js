be1f1a765cae0053bf28a9615aedda19cdc5ceee
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-06 12:02:55 -0400

    feat: integrate favoritos and bebidas stores into modal store

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -1,7 +1,11 @@
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { useFavoritosStore } from './favoritos'
+import { useBebidasStore } from './bebidas'
 
 export const useModalStore = defineStore('modal', () => {
+  const favoritos = useFavoritosStore()
+  const bebidas = useBebidasStore()
   const modal = ref(false)
 
   function handleClickModal() {
