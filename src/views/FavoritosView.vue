82f73940a0e2101c867adceca682907c64dd009b
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-06 11:05:00 -0400

    feat: implement FavoritosView component to display favorite recipes

--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -1,5 +1,14 @@
-<script setup></script>
+<script setup>
+import Receta from '@/components/RecetasCard.vue'
+import { useFavoritosStore } from '../stores/favoritos'
+
+const favoritos = useFavoritosStore()
+</script>
 
 <template>
   <h1 class="text-6xl font-extrabold">Favorites</h1>
+
+  <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 mt-10 my-10 gap-10">
+    <Receta v-for="receta in favoritos.favoritos" :receta="receta" :key="receta.idDrink" />
+  </div>
 </template>
