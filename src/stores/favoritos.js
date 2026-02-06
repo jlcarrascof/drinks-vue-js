c85fd5e8f68489baaead99dbc70cc046d8e544d3
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-06 10:03:24 -0400

    feat: initialize favoritos state in favoritos store

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,14 +1,17 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { useBebidasStore } from './bebidas'
 
 export const useFavoritosStore = defineStore('favoritos', () => {
   const bebidas = useBebidasStore()
+  const favoritos = ref([])
 
   const handleClickFavorito = () => {
     console.log(bebidas.receta)
   }
 
   return {
+    favoritos,
     handleClickFavorito,
   }
 })
