186e7d722afd4bde7684624e7afd33df6c7f135e
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-02-10 21:33:22 -0400

    feat: implement generarReceta function in IA store to call IAService

---
+++ src/services/IAService.js
@@ -0,0 +1,5 @@
+export default {
+  async generarReceta(prompt) {
+    console.log(prompt)
+  },
+}
--- src/stores/ia.js
+++ src/stores/ia.js
@@ -1,11 +1,14 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import IAService from '@/services/IAService'
 
 export const useIAStore = defineStore('ia', () => {
   const prompt = ref('')
   const respuesta = ref('')
 
-  async function generarReceta() {}
+  async function generarReceta() {
+    await IAService.generarReceta(prompt.value)
+  }
 
   return {
     prompt,
